47b780ca1a897071d9a7e3f5c2d7ec61103fc6fb
Packter Aharon <ronipakter@gmail.com>
2018-12-23 23:22:30 +0200

    good start!

---
+++ Project01_0508_9502_dotNet5779/BO/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class Enum
+    {
+        enum vehicleType { twoWheeled , privat, lightTrack, heavyTrack};
+        enum gearBox { automatic, manual };
+        enum gender { male, female };
+    }
+}
